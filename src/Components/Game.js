3c79e240e6af12effab76f2b2ef48581e1c6309c
Chappll <68033975+Chappll@users.noreply.github.com>
2021-03-18 20:00:41 +0000

    3x3 game fully working
    added an alert to notify the user has won

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import {useState} from 'react'
 import Board from './Board'
+import Alert from '@material-ui/lab/Alert'
 
 function Game(boardSize) {
     boardSize = 3
     const[board, setBoard] = useState([{position: [0,0],mark: '.',blocked: false}, {position: [1,0],mark: 'O',blocked: false}, {position: [2,0],mark: 'O',blocked: false}, {position: [0,1],mark: 'O',blocked: false}, {position: [1,1],mark: 'O',blocked: false}, {position: [2,1],mark: 'O',blocked: false}, {position: [0,2],mark: 'O',blocked: false}, {position: [1,2],mark: 'O',blocked: false}, {position: [2,2],mark: 'O',blocked: false}])
-    const changeTile = (position) =>{
-        setBoard(board.map((tile) => tile.position === position ? {...tile, mark: markChange(tile)}: tile))
-        illegalities(board)
-    }
+    const[mistakes, setMistakes] = useState()
+    const[complete, setComplete] = useState(false)
+   
 
 
     const markChange = (tile) =>{
@@ -24,18 +24,16 @@ function Game(boardSize) {
                 case 'X':
                     return '.'
                 default:
-                    return'.'
+                    return'-'
                 // code block               
             }           
         }
         return tile.mark
     }
 
-    const isLegal = () =>{
-        return illegalities(board).length == 0
-    }
 
     const illegalities = (board) =>{
+        console.log(board)
         var problems = []
         for (let i = 0; i < boardSize*boardSize; i++) {
                 if(board[i].mark != 'O' && board[i].mark != 'X')
@@ -46,25 +44,25 @@ function Game(boardSize) {
                         //Horizontal Check
                         if (board[i].position[0] > 0 && board[i].position[0] < (boardSize-1)) {
                             if (board[i-1].mark == board[i].mark && board[i].mark == board[i+1].mark) {
-                                problems.push("Horizontal row of three at " + board[i-1].position + " " + board[i].position + " " + board[i+1].position)
+                                problems.push("Horizontal row of three at (" + board[i-1].position + ") (" + board[i].position + ") (" + board[i+1].position + ")")
                                 console.log("HORIZONTAL")
                             }
                         }
                         //Vertical Check
                         if (board[i].position[1] > 0 && board[i].position[1] < (boardSize-1)) {
                             if (board[i-boardSize].mark == board[i].mark && board[i].mark == board[i+boardSize].mark) {
-                                problems.push("Vertical row of three at " + board[i-boardSize].position + " " + board[i].position + " " + board[i+boardSize].position)
+                                problems.push("Vertical row of three at (" + board[i-boardSize].position + ") (" + board[i].position + ") (" + board[i+boardSize].position + ")")
                                 console.log("VERTICLE")
                             }
                         }
                         //Diagonal Checks
                         if (board[i].position[1] > 0 && board[i].position[1] < (boardSize-1) && board[i].position[0] > 0 && board[i].position[0] < (boardSize-1)) {
                                 if (board[i-boardSize-1].mark == board[i].mark && board[i].mark == board[i+boardSize+1].mark) {
-                                    problems.push("Diagonal row of three at " + board[i-boardSize-1].position + " " + board[i].position + " " + board[i+boardSize+1].position)
+                                    problems.push("Diagonal row of three at (" + board[i-boardSize-1].position + ") (" + board[i].position + ") (" + board[i+boardSize+1].position + ")")
                                     console.log("DIAGONAL")
                                 }
                                 if (board[i+boardSize-1].mark == board[i].mark && board[i].mark == board[i-boardSize+1].mark) {
-                                    problems.push("Diagonal row of three at " + board[i+boardSize-1].position + " " + board[i].position + " " + board[i-boardSize+1].position)
+                                    problems.push("Diagonal row of three at (" + board[i+boardSize-1].position + ") (" + board[i].position + ") (" + board[i-boardSize+1].position + ")")
                                     console.log("DIAGONAL")
                                 }
                             
@@ -72,7 +70,22 @@ function Game(boardSize) {
                         }
                     }  
                     console.log(problems)     
+                    setMistakes(problems)
                     return problems
+        }
+
+        const changeTile = (position) =>{  
+            setBoard(board.map((tile) => tile.position == position ? {...tile, mark: markChange(tile)}: tile)) 
+            const loc = position[0] + (boardSize*position[1]) 
+            let tempBoard = board   
+            tempBoard[loc].mark = markChange(tempBoard[loc])    
+            let m = illegalities(tempBoard) 
+            if (checkWin(tempBoard, m)) {
+                setComplete(true) 
+                setBoard(board.map((tile) => true ? {...tile, blocked: true}: tile)) 
+            }
+                     
+            //console.log(illegalities(board))
         }
     
         const createBoard = (boardSize, numBlocked, numX, numO) =>{
@@ -85,25 +98,37 @@ function Game(boardSize) {
                 }
             }
 
-            var testBoard = createdBoard;
-           // while(illegalities(testBoard).length != 0) {
+            var testBoard = createdBoard
+            var firstCreate = true
+            while(illegalities(testBoard).length != 0 || firstCreate == true) {
+                firstCreate = false
+                testBoard = createdBoard;
                 for (let i = 0; i < numBlocked; i++) {
                     randomPos = randomInteger(0, (boardSize*boardSize) -1)
+                    while(testBoard[randomPos].mark != '.') {
+                        randomPos = randomInteger(0, (boardSize*boardSize) -1)
+                    }
                     console.log(randomPos)
                     testBoard[randomPos].mark = '-'
                     testBoard[randomPos].blocked = true              
                 }
                 for (let i = 0; i < numX; i++) {
                     randomPos = randomInteger(0, (boardSize*boardSize)-1)
+                    while(testBoard[randomPos].mark != '.') {
+                        randomPos = randomInteger(0, (boardSize*boardSize) -1)
+                    }
                     testBoard[randomPos].mark = 'X'
                     testBoard[randomPos].blocked = true       
                 }
                 for (let i = 0; i < numO; i++) {
                     randomPos = randomInteger(0, (boardSize*boardSize)-1)
+                    while(testBoard[randomPos].mark != '.') {
+                        randomPos = randomInteger(0, (boardSize*boardSize) -1)
+                    }
                     testBoard[randomPos].mark = 'O'
                     testBoard[randomPos].blocked = true    
                 }
-            //}     
+            }     
             createdBoard = testBoard
             return createdBoard
         }
@@ -115,11 +140,33 @@ function Game(boardSize) {
         function randomInteger(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
           }
+        
+        const checkWin = (wboard, m) =>{
+            let wonBoard = true
+            let i = 0
+            if (m.length != 0) {
+                wonBoard = false
+            }
+            while (i < wboard.length && wonBoard) {
+                if (wboard[i].mark != 'O' && wboard[i].mark != 'X') {
+                    if (!wboard[i].blocked) {
+                        wonBoard = false
+                    }
+                }
+                i++
+            }
+            return wonBoard
+        }
 
     return (
         <div>
             <Board board={board} changeTile={changeTile} />
             <button onClick={() => setNewRandomBoard(3,1,1,1)}>{"Create New Board"}</button>
+            <form>{mistakes}</form>
+            {complete ? <Alert severity="success" color="info">
+            This is a success alert — check it out!
+            </Alert>: null}
+            
         </div>
     )
 }
--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -4,11 +4,11 @@ const Tile = ({tile, changeTile}) => {
     // const id = 1;
     // const[noughtCross, setNoughtCross] = null;
     // const[blocked,setBlocked] = false;
-    
+   // {((tile.position[0]+tile.position[1])%2==0)?'tileButton':'darkBlueTileButton'}
     return (
         <div className='tile'>
-            <button className={((tile.position[0]+tile.position[1])%2==0)?'tileButton':'darkBlueTileButton'} onClick={() => changeTile(tile.position)}>{tile.mark}</button>
-           
+            <button className={tile.blocked?'tileButton':'darkBlueTileButton'} onClick={() => changeTile(tile.position)}>{tile.mark}</button>
+            
         </div>
     )
 }
